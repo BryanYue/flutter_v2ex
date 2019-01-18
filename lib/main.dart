816153da7c39fea1a4b9833dbc3c38c87ee61a9e
BryanYue <phantom3792@gmail.com>
2019-01-18 15:17:35 +0800

    基本小部件  认识StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 //导入包
 import 'package:flutter/material.dart';
+import 'device/device.dart';
+
 
 //应用入口
-void main() => runApp(new MyApp());
+void main() async{
+  device().get();
+  runApp(new MyApp());
+}
 
 //应用结构
 
@@ -16,7 +21,10 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Echo(text: "hello, world" ,backgroundColor: Colors.blue);
+    return new MaterialApp(
+      title: 'V2EX', // used by the OS task switcher
+      home: MyScaffold(),
+    );
   }
 
 
@@ -45,3 +53,67 @@ class Echo extends StatelessWidget {
   }
 }
 
+
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        // <Widget> is the type of items in the list.
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
